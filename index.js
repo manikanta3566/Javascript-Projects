5038c45d699a122bc4db9ecf06ae91342d818bd4
Manikanta <manikantapoojary444@gmail.com>
2021-11-24 18:10:51 +0530

    Add button code finished

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log("welcome");
+//if user adds notes add to the local storage
+let addBtn=document.getElementById("addBtn");
+addBtn.addEventListener("click",addbtn)
+function addbtn(e)
+{
+let addText=document.getElementById("addText");
+let notes=localStorage.getItem("notes");
+if(notes==null){
+    notesobj=[];
+}
+else{
+    notesobj=JSON.parse(notes);
+}
+
+//adding the entered text to the notesobj array
+notesobj.push(addText.value);
+
+//adding notes to the local storage
+localStorage.setItem("notes",JSON.stringify(notesobj));
+console.log(notesobj);
+shownotes();//function to show the notes when addText button fired
+};
